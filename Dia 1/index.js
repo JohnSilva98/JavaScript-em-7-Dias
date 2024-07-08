7922c679a70e7355616209ab50242f6a3d27ccfa
JohnSilva98 <jonathan.design16@oulook.com>
2024-07-08 11:55:33 -0300

    conhecendo as variaveis tipo referencia, criando obejtos

--- Dia 1/index.js
+++ Dia 1/index.js
@@ -13,3 +13,34 @@ const price = 30;
 //price = 20;
 console.log(price);
 //variaveis constantes nunca terão seu valor alterado, o valor dela se mantem fixo(constante)
+
+//variaveis de tipo primitivo
+//string, number, boolean, undefined, null, symbol
+
+let itemName = "pen"; // variavel tipo String
+let itemPrice = 10; // variavel tipo Number
+let isAvailable = true; // variavel tipo Boolean
+let itemColor; //variavel tipo undefined
+// variaveis de tipo primitivo são imutáveis, ou seja, não podem ser
+// alteradas, o valor da variavel é fixo e não pode ser alterado
+// variaveis de tipo primitivo são passadas por valor, ou seja, quando
+// passamos uma variavel de tipo primitivo para outra variavel, o valor
+// da variavel é copiado para a outra variavel, ou seja, as duas
+// variaveis tem o mesmo valor, mas são diferentes variaveis
+
+//variaveis do tipo referencia
+//object, array, function, date, regExp, error
+let pen = {
+  itemName: "Pen",
+  itemPrice: 3,
+  isAvailable: true,
+  itemColor: "red",
+};
+pen.itemColor = "blue";
+console.log(pen);
+// variaveis de tipo referencia são mutáveis, ou seja, podem ser
+// alteradas, o valor da variavel pode ser alterado
+// variaveis de tipo referencia são passadas por referência, ou seja,
+// quando passamos uma variavel de tipo referencia para outra variavel,
+// o valor da variavel é copiado para a outra variavel, ou seja, as duas
+// variaveis tem o mesmo valor, mas são diferentes variaveis
