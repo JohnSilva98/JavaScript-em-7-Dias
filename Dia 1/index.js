9ae434cbd412535a4277c299485231ebc22944ba
JohnSilva98 <jonathan.design16@oulook.com>
2024-07-08 11:08:09 -0300

    conhecendo as variáveis let

--- Dia 1/index.js
+++ Dia 1/index.js
@@ -1,1 +1,10 @@
 console.log("JavaScript funcionando");
+
+let firstName = "John";
+let lastName = "Silva";
+console.log(firstName + " " + lastName);
+//variaveis nao podem começar com números, nem ter espaços, nem hífens
+// uma boa prática é usar a primeira letra da
+//primeira palavra em minúsculo
+// a segunda letra do nome é maiuscula
+//JS é case sensitive, ou seja, letra maiuscula e minúsculas são diferentes
