0c4522b8d74b2bf051b0248dc2513385248bf9cf
JohnSilva98 <jonathan.design16@oulook.com>
2024-07-11 11:25:55 -0300

    finalizando dia 2, modificando o código para trocar palavras automaticamente

--- Dia 2/index.js
+++ Dia 2/index.js
@@ -1,1 +1,18 @@
+const foods = ["burguer", "pizza", "pasta"];
 
+function changeWords() {
+  let index = 0;
+  const text = document.getElementById("text");
+
+  setInterval(() => {
+    // Aplica a classe 'hidden' para iniciar o fade out
+    text.classList.add("hidden");
+    setTimeout(() => {
+      text.innerHTML = foods[index];
+      text.classList.remove("hidden");
+      index = (index + 1) % foods.length;
+    }, 500);
+  }, 3000);
+}
+
+changeWords();
