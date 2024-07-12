31dba54e3d7bb34d6b826b36971d61f3037633fd
JohnSilva98 <jonathan.design16@oulook.com>
2024-07-12 11:44:26 -0300

    desafio finalizado

--- Dia 3/index.js
+++ Dia 3/index.js
@@ -1,33 +1,46 @@
-function createBook(title, author, pages, year) {
-  //factory
-  const book = {
-    bookTitle: title,
-    author: author,
-    bookPages: pages,
-    bookYear: year,
-    printBook: function () {
-      console.log("printing...");
-    },
-  };
-  return book;
+// function createBook(title, author, pages, year) {
+//   //factory
+//   const book = {
+//     bookTitle: title,
+//     author: author,
+//     bookPages: pages,
+//     bookYear: year,
+//     printBook: function () {
+//       console.log("printing...");
+//     },
+//   };
+//   return book;
+// }
+
+// function CreateBook(title, author, pages, year) {
+//   //constructor
+
+//   this.bookTitle = title;
+//   this.author = author;
+//   this.ookPages = pages;
+//   this.bookYear = year;
+// }
+
+// const book1 = createBook("Atomic Habits", "James Clear", 306, 2018);
+// const book2 = new CrpeateBook(
+//   "Think and grow rich",
+//   "Napolean Ceasar",
+//   450,
+//   2010
+// );
+
+// console.log(book1);
+// console.log(book2);
+const resultSpan = document.getElementById("result");
+let value = Number(prompt("Enter the product value: "));
+console.log(typeof value);
+
+if (value >= 20) {
+  resultSpan.innerHTML = "Aproved";
+  resultSpan.classList.add("green");
+  resultSpan.classList.remove("red");
+} else {
+  resultSpan.innerHTML = "Not Aproved";
+  resultSpan.classList.add("red");
+  resultSpan.classList.remove("green");
 }
-
-function CreateBook(title, author, pages, year) {
-  //constructor
-
-  this.bookTitle = title;
-  this.author = author;
-  this.ookPages = pages;
-  this.bookYear = year;
-}
-
-const book1 = createBook("Atomic Habits", "James Clear", 306, 2018);
-const book2 = new CrpeateBook(
-  "Think and grow rich",
-  "Napolean Ceasar",
-  450,
-  2010
-);
-
-console.log(book1);
-console.log(book2);
