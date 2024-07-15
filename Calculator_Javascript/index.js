cafade5276bdcb617ac3117cb2fcc8acba7efdd9
JohnSilva98 <jonathan.design16@oulook.com>
2024-07-15 14:13:30 -0300

    criado calculadora simples

--- Calculator_Javascript/index.js
+++ Calculator_Javascript/index.js
@@ -1,5 +1,20 @@
 function outcome() {
-  let num1 = document.getElementById("num-one").value;
-  let num2 = document.getElementById("num-two").value;
+  let num1 = Number(document.getElementById("num-one").value);
+  let num2 = Number(document.getElementById("num-two").value);
   let total = 0;
+
+  let add = document.getElementById("box1");
+  let subtract = document.getElementById("box2");
+  let multiply = document.getElementById("box3");
+
+  if (add.checked) {
+    total = num1 + num2;
+  } else if (subtract.checked) {
+    total = num1 - num2;
+  } else if (multiply.checked) {
+    total = num1 * num2;
+  } else {
+    total = num1 / num2;
+  }
+  document.getElementById("resultArea").innerHTML = "Result: " + String(total);
 }
